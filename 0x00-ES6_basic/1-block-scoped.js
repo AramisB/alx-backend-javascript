f6ccce9c8340e6794b80ae47f04db260ba526900
AramisB <brendachem7@gmail.com>
2024-07-22 14:49:29 +0300

    hoisted let and const

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+//modify the variables inside the function taskBlock so that the variables aren’t overwritten inside the conditional block.
+export default function taskBlock(trueOrFalse) {
+    let task = false;
+    let task2 = true;
+  
+    if (trueOrFalse) {
+      let task = true;
+      let task2 = false;
+    }
+  
+    return [task, task2];
+  }
