a2aec3043b74e1601fc5c968f12a08ee453dae0e
AramisB <brendachem7@gmail.com>
2024-07-24 18:36:22 +0300

    throw an error

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,21 @@
+// Write a function named guardrail that will accept one argument mathFunction (Function).
+
+// This function should create and return an array named queue.
+
+// When the mathFunction function is executed,
+// the value returned by the function should be appended to the queue.
+// If this function throws an error, the error message should be appended to the queue.
+// In every case, the message Guardrail was processed should be added to the queue.
+
+export default function guardrail(mathFunction) {
+  const queue = [];
+  const message = 'Guardrail was processed';
+  try {
+    const result = mathFunction();
+    queue.push(result, message);
+    return queue;
+  } catch (error) {
+    queue.push(error.message, message);
+    return queue;
+  }
+}
