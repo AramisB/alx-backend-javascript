b252c71b74003bb9041dd7a1c97fb27a8125dbb0
AramisB <brendachem7@gmail.com>
2024-07-24 18:39:19 +0300

    throw an error

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -12,10 +12,12 @@ export default function guardrail(mathFunction) {
   const message = 'Guardrail was processed';
   try {
     const result = mathFunction();
-    queue.push(result, message);
+    queue.push(result);
     return queue;
   } catch (error) {
-    queue.push(error.message, message);
-    return queue;
+    queue.push(error.message);
+  } finally {
+    queue.push(message);
   }
+  return queue;
 }
