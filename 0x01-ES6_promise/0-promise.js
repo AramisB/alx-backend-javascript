cf274e15677bcd38915fed94e76363eab4fef0cf
AramisB <brendachem7@gmail.com>
2024-07-24 17:19:33 +0300

    rejected promises

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,7 @@
 // Return a Promise using this prototype: function getResponseFromAPI()
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve('Success!');
-    }, 1000);
+  return Promise.resolve({
+    status: 200,
+    body: 'Success',
   });
 }
