c17d4a203579b0cf0bbac1096f389457b3198d90
AramisB <brendachem7@gmail.com>
2024-07-24 18:54:26 +0300

    use finally regardess of promise status

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -11,9 +11,7 @@
 
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => {
-      console.log('Got a response from the API to the console');
-      return { status: 200, body: 'success' };
-    })
-    .catch(() => new Error());
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
