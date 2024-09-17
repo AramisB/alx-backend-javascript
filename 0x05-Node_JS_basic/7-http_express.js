39f522a62d2477d1f526bcc5447e869e47f8a54f
AramisB <brendachem7@gmail.com>
2024-09-17 16:12:58 +0300

    advanced express()

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,24 +1,24 @@
 const express = require('express');
 
 const args = process.argv.slice(2);
-
-const app = express();
-const port = 1245;
 const countStudents = require('./3-read_file_async');
 
 const DB = args[0];
+
+const app = express();
+const port = 1245;
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
-  const message = 'This is the list of our students\n';
+  const intro = 'This is the list of our students\n';
   try {
-    const students = countStudents(DB);
-    res.send(`${message}${students}`);
+    const students = await countStudents(DB);
+    res.send(`${intro}${students.join('\n')}`);
   } catch (error) {
-    res.send(error.message);
+    res.send(`${intro}${error.message}`);
   }
 });
 
