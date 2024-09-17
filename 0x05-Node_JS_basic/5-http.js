8f777535e46307ec58789c1800e41508a4877762
AramisB <brendachem7@gmail.com>
2024-09-17 14:56:47 +0300

    a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,26 +1,40 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
+const countStudents = require('./3-read_file_async'); // Reuse the logic from 3-read_file_async.js
 
-const server = http.createServer((req, res) => {
+// Create a server
+const app = http.createServer((req, res) => {
   if (req.url === '/') {
+    // Route for "/"
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
+    // Route for "/students"
     res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-    countStudents('./database.csv')
-      .then((content) => {
-        res.end(`This is the list of our students\n${content}`);
-      })
-      .catch((error) => {
-        res.end(error.message);
-      });
+    // Retrieve the student list from a CSV file
+    const databasePath = './database.csv'; // Make sure this file exists in the same directory
+    if (fs.existsSync(databasePath)) {
+      countStudents(databasePath)
+        .then((content) => {
+          res.end(`This is the list of our students\n${content}`);
+        })
+        .catch((error) => {
+          res.end(error.message);
+        });
+    } else {
+      // If the database is missing
+      res.end('This is the list of our students\nCannot load the database');
+    }
   } else {
+    // 404 - Not Found for other routes
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found');
   }
 });
 
-server.listen(1245);
+// Server listening on port 1245
+app.listen(1245);
 
-module.exports = server;
+// Export the app (useful for testing)
+module.exports = app;
