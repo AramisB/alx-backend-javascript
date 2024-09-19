d5897132b7eb4dadbd25e2ac5c8ea7d845a9898d
AramisB <brendachem7@gmail.com>
2024-09-19 23:10:09 +0300

    model.exports utils

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,17 +1,16 @@
-export const Utils = {
-    calculateNumber(type, a, b) {
-      const roundedA = Math.round(a);
-      const roundedB = Math.round(b);
-  
-      if (type === 'SUM') {
-        return roundedA + roundedB;
-      } else if (type === 'SUBTRACT') {
-        return roundedA - roundedB;
-      } else if (type === 'DIVIDE') {
-        if (roundedB === 0) {
-          return 'Error';
-        }
-        return roundedA / roundedB;
-      }
+const Utils = {
+  calculateNumber(type, a, b) {
+    if (type === 'SUM') {
+      return Math.round(a) + Math.round(b);
     }
-  };
+    if (type === 'SUBTRACT') {
+      return Math.round(a) - Math.round(b);
+    }
+    if (type === 'DIVIDE') {
+      return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
+    }
+    return 0;
+  },
+};
+
+module.exports = Utils;
